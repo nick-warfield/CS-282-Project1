440cccb1a3fede0ba74d8eb53c09afc5e75ab4b6
javier-aguayo <javieraguayo95@gmail.com>
2019-02-23 12:19:49 -0800

    testing branch

--- DataItem.java
+++ DataItem.java
@@ -1,5 +1,6 @@
 public interface DataItem<E> extends Comparable<E> {
    
    public boolean memberOfGroup(int n); 
-   public String toString();   
+   public String toString(); 
+   public int Test1;
 }
