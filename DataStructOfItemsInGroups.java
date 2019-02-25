51837d08c872e586c71cea5f9d9b5a1aa4d9faee
John Wiesenfeld <jawiesenfeld@gmail.com>
2019-02-25 13:56:19 -0800

    inital creation of classes

--- DataStructOfItemsInGroups.java
+++ DataStructOfItemsInGroups.java
@@ -1,12 +1,12 @@
 public interface DataStructOfItemsInGroups <E extends DataItem<E>> {
 
-   public void insert(E item);
-   public void delete(E item);
-   public E find(E item);
-   public int numInGroup(int num);
-   public int sizeLargest();
-   public int sizeSmallest();
-   public String members(int num);
-   public int numToReachAll();
+    public void insert(E item);
+    public void delete(E item);
+    public E find(E item);
+    public int numInGroup(int num);
+    public int sizeLargest();
+    public int sizeSmallest();
+    public String members(int num);
+    public int numToReachAll();
 
 }
---
+++ Student.java
@@ -0,0 +1,44 @@
+public class Student{
+
+    public long idNumber;
+    public String name;
+    public boolean[] inGroup;
+
+    public Student(long idNumber, String name, boolean[] inGroup)
+    {
+        this.idNumber = idNumber;
+        this.name = name;
+        this.inGroup = inGroup;
+    }
+
+    public void print()
+    {
+        System.out.print(idNumber() + ", ");
+        System.out.print(name() + ", ");
+        for(int i = 0; i < inGroup.length; i++)
+        {
+            if(inGroup[i] == true)
+            {
+                System.out.print("T");
+            } else {
+                System.out.print("F");
+            }
+        }
+    }
+
+    public long getIdNumber()
+    {
+        return idNumber;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public boolean[] getInGroup()
+    {
+        return inGroup;
+    }
+
+}
