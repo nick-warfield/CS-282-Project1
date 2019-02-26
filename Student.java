07ac02965e279628cf57223a9ee26814b5b7baa1
John Wiesenfeld <jawiesenfeld@gmail.com>
2019-02-25 16:48:51 -0800

    John edited comment on Student.java

--- Student.java
+++ Student.java
@@ -30,7 +30,7 @@ public class Student implements DataItem<Student> {
     /*
     compareTo(Student s) will return 0 if student ID #s are equal, otherwise it will return the difference
     between them. This is the same implementation as compareTo from the java.lang package except made to work
-    with Student ID numbers here
+    with Student ID numbers here instead of Strings.
      */
     public int compareTo(Student s)
     {
