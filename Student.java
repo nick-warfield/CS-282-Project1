39030229138861a0bad0bf49251b44c18776ccea
John Wiesenfeld <jawiesenfeld@gmail.com>
2019-02-25 16:09:21 -0800

    first draft of Student.java

--- Student.java
+++ Student.java
@@ -1,4 +1,4 @@
-public class Student implements DataItem<E>{
+public class Student implements DataItem<Student> {
 
     public long idNumber;
     public String name;
@@ -11,19 +11,36 @@ public class Student implements DataItem<E>{
         this.inGroup = inGroup;
     }
 
-    public void print()
+    public String toString()
     {
-        System.out.print(idNumber() + ", ");
-        System.out.print(name() + ", ");
+        String string = idNumber + ", " + name + ", ";
         for(int i = 0; i < inGroup.length; i++)
         {
             if(inGroup[i] == true)
             {
-                System.out.print("T");
+                string += "T";
             } else {
-                System.out.print("F");
+                string += "F";
             }
         }
+
+        return string;
+    }
+
+    /*
+    compareTo(Student s) will return 0 if student ID #s are equal, otherwise it will return the difference
+    between them. This is the same implementation as compareTo from the java.lang package except made to work
+    with Student ID numbers here
+     */
+    public int compareTo(Student s)
+    {
+         int comp = (int)(this.idNumber - s.idNumber);
+         return comp;
+    }
+
+    public boolean memberOfGroup(int group)
+    {
+        return inGroup[group];
     }
 
     public long getIdNumber()
